e428a13cf2d3e50a6873c6a8a7a4ec72d7ed0b9f
736659711@qq.com <736659711@qq.com>
2020-03-22 20:51:50 +0800

    .

--- Data_collect/ID/a.py
+++ Data_collect/ID/a.py
@@ -1,10 +1,3 @@
-x2 = []
-with open('users.txt','r') as f:
-    x = f.read().split('\n')
-    print(len(x))
-    x2 = list(set(x))
-    print(len(x2))
-
-with open('users3.txt','a') as f:
-    x = '\n'.join(x2)
-    f.write(x)
+a = {}
+print(a['a'])
+print(a)
